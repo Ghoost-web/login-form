40335ff9ed8d579b6aee04a5e45b166c15795029
ahmed-osama2022 <ahmadosama.2011@gmail.com>
2023-01-23 21:18:18 +0200

    adding the local storage key

--- js/main.js
+++ js/main.js
@@ -9,15 +9,17 @@
  */
 const form = document.querySelector('.form');
 const darkBtn = document.getElementById('dark-night-btn');
+const dayBtn = document.getElementById('day-night-btn');
 const darkBtnImg = document.getElementById('dark-night-btn-img');
 
+// For local-storage
+const darkLocal = localStorage.getItem('dark');
+
+
 // Fot test only
-console.log(form);
-console.log(darkBtn);
-console.log(darkBtnImg);
-
-
-
+// console.log(form);
+// console.log(darkBtn);
+// console.log(darkBtnImg);
 
 /**
  * End global variables
@@ -27,29 +29,48 @@ console.log(darkBtnImg);
 /**
  * Start helper functions
  */
+
+// Start check for the last status from local storage of the user
+if (darkLocal !== 'enabled') {
+    dayMode();
+} else {
+    darkMode();
+};
+
+
+// helper functions day-night switcher
 function darkMode() {
+    localStorage.setItem('dark', 'enabled');
+    
     form.classList.remove('light');
     form.classList.add('dark');
-    
-    darkBtn.classList.remove('light');
-    darkBtn.classList.add('dark');
-    darkBtn.classList.add('invert');
-    
-    darkBtnImg.classList.add('rotate');
 
-    
+    dayBtn.style.display = 'block';
+    darkBtn.style.display  = 'none';
+
+    dayBtn.classList.remove('light');
+    dayBtn.classList.add('dark');
+    dayBtn.classList.add('invert');
 }
+function dayMode() {
+    localStorage.setItem('dark', null);
 
+    form.classList.add('light');
+    form.classList.remove('dark');
 
+    darkBtn.style.display = 'block';
+    dayBtn.style.display  = 'none';
 
-
+    dayBtn.classList.add('light');
+    dayBtn.classList.remove('dark');
+    dayBtn.classList.remove('invert');
+}
+// End day-night switcher functions
+ 
 
 /**
  * End helper functions
  */
-
-
-
 
 
 
