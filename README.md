c11100b301e547dde041ccd721da2018bc79dde9
ahmed-osama2022 <ahmadosama.2011@gmail.com>
2023-01-23 20:09:16 +0200

    Adding the dark mode switch.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Login-form
 
+## Sign-up && Sign-in login page
+
+---
+## Creator 
+
+<p>This page was created by &copy;GhostWeb Design company</p>
+<p>All rights reserved 2023</p>
--- js/main.js
+++ js/main.js
@@ -4,13 +4,49 @@
  */
 
 
+/**
+ * Start global variables
+ */
+const form = document.querySelector('.form');
+const darkBtn = document.getElementById('dark-night-btn');
+const darkBtnImg = document.getElementById('dark-night-btn-img');
+
+// Fot test only
+console.log(form);
+console.log(darkBtn);
+console.log(darkBtnImg);
+
+
+
+
+/**
+ * End global variables
+ */
+
+
+/**
+ * Start helper functions
+ */
+function darkMode() {
+    form.classList.remove('light');
+    form.classList.add('dark');
+    
+    darkBtn.classList.remove('light');
+    darkBtn.classList.add('dark');
+    darkBtn.classList.add('invert');
+    
+    darkBtnImg.classList.add('rotate');
+
+    
+}
 
 
 
 
 
-
-
+/**
+ * End helper functions
+ */
 
 
 
